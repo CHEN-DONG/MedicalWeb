fe56e6fbfe9ece0ef67b36605bc73d00518da713
谢陈东 <chendong.wings@gmail.com>
2018-03-30 00:15:43 +0800

    add pages

--- js/home.js
+++ js/home.js
@@ -1,6 +1,19 @@
-window.onload = function(){
+window.onload = function () {
   $('.ui.card .image').dimmer({
     on: 'hover'
   });
   $('.menu .item').tab();
+  $('.ui.calendar').calendar({
+    type: "date",
+    formatter: {
+      date: function (date, settings) {
+        if (!date) return '';
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+        return year + '-' + month + '-' + day;
+      }
+    }
+  });
+  $('.ui.dropdown').dropdown();
 }
